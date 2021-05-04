bbe5c19f07b31284f8d451bf18b7c24934422bd0
Nikhil Arora <nikhil290192@gmail.com>
2021-05-05 01:46:55 +0530

    CheckVowels.js is Added

--- String/CheckVowels.js
+++ String/CheckVowels.js
@@ -7,7 +7,6 @@ const checkVowels = (value)  => {
     if (typeof value !== 'string') {
         throw new TypeError('The first param should be a string')
     }
-
     const vowels = ['a', 'e', 'i', 'o', 'u']
     let countVowels = 0
     for(let i = 0; i < value.length; i++) {
@@ -16,7 +15,6 @@ const checkVowels = (value)  => {
             countVowels++
         }
     }
-
     return countVowels
 }
 
